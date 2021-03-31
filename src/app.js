2d3922534dee0e2eaff3419abfbcc723f3d3fe64
KalebeLopes <calbkalebe@gmail.com>
2021-03-31 10:34:05 -0300

    refactor: acl and middlewares configure

--- src/app.js
+++ src/app.js
@@ -2,11 +2,20 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import routes from './routes'
 import database from "./database"
+import acl from "express-acl"
+import authMiddleware from "./middlewares/auth"
 
 const app = express()
 
+acl.config({
+  baseUrl: "/",
+  path: "config"
+})
+
 const configureExpress = () => {
   app.use(bodyParser.json())
+  app.use(authMiddleware)
+  app.use(acl.authorize.unless({ path: ["/users/authenticate"] }))
   app.use('/', routes)
   app.database = database
 
