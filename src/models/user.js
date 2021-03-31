6874574c670db1aee98d779c176bb3a94e8c3c6e
KalebeLopes <calbkalebe@gmail.com>
2021-03-31 07:50:15 -0300

    refactor: creating middleware to encrypt the users password

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,25 @@
 import mongoose from 'mongoose'
+import Util from "util"
+import bcrypy from "bcrypt"
 
+const hashAsync = Util.promisify(bcrypy.hash)
 const schema = new mongoose.Schema({
   name: String,
   cpf: String,
   password: String,
   role: String
+})
+
+schema.pre("save", async function(next) { // middleware para interceptar o método save() do mongoose
+  if (!this.password || !this.isModified("password")) {
+    return next()
+  }
+  try {
+    const hashedPassword = await hashAsync(this.password, 10)
+    this.password = hashedPassword;
+  } catch (err) {
+    next(err)
+  }
 })
 
 schema.set("toJSON", { // utilizado para intervir na serialização e customizar o resultado final do obj
