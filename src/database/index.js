9c1ff94060cc949b45a36ffdcbba1e8d0389d82c
jpcirqueira <jpcirqueira81@gmail.com>
2021-03-30 22:20:54 -0300

    adicionado migrate de usuario.

--- src/database/index.js
+++ src/database/index.js
@@ -4,11 +4,4 @@ const dbConfig = require('../config/database.js');
 
 const connection = new Sequelize(dbConfig);
 
-try {
-  connection.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
-
 module.exports = connection;
