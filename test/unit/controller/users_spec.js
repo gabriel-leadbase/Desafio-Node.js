fa04329cac7fa4c8764fbe09a1dfb44ffe85e16c
KalebeLopes <calbkalebe@gmail.com>
2021-03-31 01:51:36 -0300

    test: create get method

---
+++ test/unit/controller/users_spec.js
@@ -0,0 +1,75 @@
+import UsersController from '../../../src/controller/users'
+import User from '../../../src/models/user'
+import sinon from 'sinon'
+
+describe('Controllers: Users', () => {
+  const defaultId = '56cb91bdc3464f14678934ca'
+  const defaultUser = [
+    {
+      __V: 0,
+      _id: defaultId,
+      name: 'Default Name',
+      cpf: '12345678901',
+      password: 'password',
+      role: 'admin'
+    }
+  ] 
+  const defaultRequest = {
+    params: {}
+  }
+
+  describe('get() users', () => {
+    it('should return a list of users', async() => {
+      const response = {
+        send: sinon.spy()
+      }
+
+      User.find = sinon.stub()
+      User.find.withArgs({}).resolves(defaultUser)
+
+      const usersController = new UsersController(User)
+      await usersController.get(defaultRequest, response)
+
+      sinon.assert.calledWith(response.send, defaultUser)
+    })
+
+    it('should return 400 when an error occour', async() => {
+      const response = {
+        send: sinon.spy(),
+        status: sinon.stub()
+      }
+
+      response.status.withArgs(400).returns(response)
+      User.find = sinon.stub()
+      User.find.withArgs({}).rejects({ message: 'Error' })
+
+      const usersController = new UsersController(User)
+      await usersController.get(defaultRequest, response)
+
+      sinon.assert.calledWith(response.send, 'Error')
+    })
+  })
+
+  describe('getById() users', () => {
+    it('should call send with one user', async () => {
+      const fakeId = 'a-fake-id'
+      const request = {
+        params: {
+          id: fakeId
+        }
+      }
+      const response = {
+        send: sinon.spy()
+      }
+
+      User.findOne = sinon.stub()
+      User.findOne.withArgs({ _id: fakeId }).resolves(defaultUser)
+
+      const usersController = new UsersController(User)
+
+      await usersController.getById(request, response)
+      sinon.assert.calledWith(response.send, defaultUser)
+    });
+  });
+
+})
