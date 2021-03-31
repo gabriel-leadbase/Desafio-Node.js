059de8dcfe8566e9045ea5a3d8a1a61fb5a19683
KalebeLopes <calbkalebe@gmail.com>
2021-03-31 02:02:18 -0300

    test: create user endpoints

---
+++ src/routes/users.js
@@ -0,0 +1,16 @@
+import express from "express"
+import UsersController from "../../src/controller/users"
+import User from "../../src/models/user"
+
+const router = express.Router()
+const usersController = new UsersController(User)
+
+router.get("/", (req, res) => {
+  usersController.get(req, res)
+})
+
+router.get("/:id", (req, res) => {
+  usersController.getById(req, res)
+})
+
+export default router;
