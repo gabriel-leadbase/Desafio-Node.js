a24faae5ea6fdc0f81bf564a00cbaa297ecc5c8d
KalebeLopes <calbkalebe@gmail.com>
2021-03-31 10:28:35 -0300

    feat: token generation for user

---
+++ src/services/auth.js
@@ -0,0 +1,27 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import config from "config"
+
+class Auth {
+  constructor(User) {
+    this.User = User
+  }
+
+  async authenticate(dataUser) {
+    const user = await this.User.findOne({ cpf: dataUser.cpf })
+    // console.log(user)
+    if (!user || !(await bcrypt.compare(dataUser.password, user.password))) {
+      console.log("Senhas não são iguais");
+      return false;
+    }
+    return user;
+  }
+
+  static generateToken(payload) {
+    return jwt.sign(payload, config.get("auth.key"), {
+      expiresIn: config.get("auth.tokenExpiresIn")
+    });
+  }
+}
+
+export default Auth;
