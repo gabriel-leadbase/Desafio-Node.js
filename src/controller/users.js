362a6bb072637604d9bed2413adc957774c8236f
KalebeLopes <calbkalebe@gmail.com>
2021-03-31 01:51:43 -0300

    feat: create get method

---
+++ src/controller/users.js
@@ -0,0 +1,29 @@
+export default class UserController {
+  constructor(User) {
+    this.User = User
+  }
+
+  async get(req, res) {
+    try {
+      const users = await this.User.find({})
+      res.send(users)
+    } catch (err) {
+      res.status(400).send(err.message)
+    }
+  }
+
+  async getById(req, res) {
+    const {
+      params: { id }
+    } = req;
+    try {
+      const user = await this.User.findOne({ _id: id });
+      // console.log(user)
+      res.send(user);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+
+  
+}
