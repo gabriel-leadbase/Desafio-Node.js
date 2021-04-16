69ff0fba934d8be66910891db7b8eb16925773f4
Raisess <raisechif@gmail.com>
2021-04-16 19:27:20 -0300

    created user controller get functions and user get routes

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -16,4 +16,32 @@ async function createUser(req, res) {
   }
 }
 
-module.exports = { createUser };
+async function getUser(req, res) {
+  try {
+    const { cpf } = req.params;
+    const user = await userModel.getUser(cpf);
+
+    if (user === undefined) throw new Error("User not found!");
+
+    return res.status(201).json({
+      log: "user getted",
+      user,
+    });
+  } catch (err) {
+    return res.status(404).json({
+      log: "error on user get",
+      error: err.message,
+    });
+  }
+}
+
+async function getUsers(req, res) {
+  const users = await userModel.getUsers();
+
+  return res.status(201).json({
+    log: "users getted",
+    users,
+  });
+}
+
+module.exports = { createUser, getUser, getUsers };
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
 
-const { createUser } = require("../controllers/users.controller");
+const {
+  createUser,
+  getUser,
+  getUsers,
+} = require("../controllers/users.controller");
 
 const userRoutes = Router();
 
 userRoutes.post("/user", createUser);
+userRoutes.get("/user", getUsers);
+userRoutes.get("/user/:cpf", getUser);
 
 module.exports = userRoutes;
