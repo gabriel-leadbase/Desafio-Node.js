3dc4a7edf6a32094ee1f44df3d3a218734df5a76
Raisess <raisechif@gmail.com>
2021-04-16 17:07:18 -0300

    created get users tests

--- src/__tests__/users.spec.js
+++ src/__tests__/users.spec.js
@@ -1,11 +1,11 @@
 require("dotenv/config");
 require("../database/connect");
 
-const { createUser } = require("../models/users.model");
+const { createUser, getUsers, getUser } = require("../models/users.model");
 
 describe("Users model teste suite", () => {
   const userData = {
-    cpf: "123",
+    cpf: "1234",
     password: "123",
     role: "ADMIN",
   };
@@ -14,5 +14,17 @@ describe("Users model teste suite", () => {
     const user = await createUser(userData);
 
     expect(user.cpf).toBe(userData.cpf);
+  });
+
+  it("get all users", async () => {
+    const users = await getUsers();
+
+    expect(users.length > 0).toBe(true);
+  });
+
+  it("get one user", async () => {
+    const user = await getUser(userData.cpf);
+
+    expect(user.role).toBe(userData.role);
   });
 });
