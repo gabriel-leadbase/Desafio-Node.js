c51eb004a4ddc579d8387fa9f4f63ac9ba11682d
Raisess <raisechif@gmail.com>
2021-04-16 17:43:15 -0300

    added json support to server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+const userRoutes = require("./routes/users.route");
+
 module.exports = function server(port) {
   const app = express();
 
   app.use(cors());
+  app.use(express.json());
+
+  app.use(userRoutes);
 
   app.listen(port, () => console.log("running on port:", port));
 };
