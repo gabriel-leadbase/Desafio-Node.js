e5106f3802e7d8ad6f04dbe4c79ca7f89ae48b76
Raisess <raisechif@gmail.com>
2021-04-17 08:37:41 -0300

    updated routers middleware injection method

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ require("dotenv/config");
 require("./database/connect");
 
 const server = require("./server");
+const userRoutes = require("./routes/users.route");
+const authRoutes = require("./routes/auth.route");
 
-server(1939);
+server(1939, [userRoutes, authRoutes]);
--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
-const userRoutes = require("./routes/users.route");
-
-module.exports = function server(port) {
+module.exports = function server(port, routers) {
   const app = express();
 
   app.use(cors());
   app.use(express.json());
 
-  app.use(userRoutes);
+  for (const router of routers) {
+    app.use(router);
+  }
 
   app.listen(port, () => console.log("running on port:", port));
 };
