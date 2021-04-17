710e453c226aefb27c15823f4f30686b785ddaa2
Raisess <raisechif@gmail.com>
2021-04-17 09:00:25 -0300

    prettier role middleware

--- src/common/middlewares/role.middleware.js
+++ src/common/middlewares/role.middleware.js
@@ -2,13 +2,15 @@ const { authUser } = require("../../models/users.model");
 
 async function roleMiddleware(req, res, next) {
   try {
-    const decodedToken = await authUser({ token: req.headers.authorization.split(" ")[1] });
+    const decodedToken = await authUser({
+      token: req.headers.authorization.split(" ")[1],
+    });
 
     if (decodedToken.role === "ADMIN") {
       next();
     } else {
       return res.status(401).json({
-        log: "invalid role"
+        log: "invalid role",
       });
     }
   } catch (err) {
