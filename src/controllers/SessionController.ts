df50ec59deee835b9643687ea95ccd0ce684a79d
Gabriel Silvares <gabrielsrcode@gmail.com>
2021-05-29 18:52:13 -0300

    👌 Modificando o tratamento de erro do session controller

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -5,11 +5,21 @@ export class SessionController {
   async create(request: Request, response: Response): Promise<Response> {
     const { cpf, password } = request.body;
 
-    const tokenAndUserData = await authenticate({
-      cpfNumber: cpf,
-      password,
-    });
+    try {
+      const { user, token } = await authenticate({
+        cpfNumber: cpf,
+        password,
+      });
 
-    return response.json(tokenAndUserData);
+      // @ts-expect-error
+      delete user.password;
+
+      return response.json({ user, token });
+    } catch (error) {
+      console.log(`Error >> ${error}`);
+      return response.status(400).json({
+        message: 'Unexpected error',
+      });
+    }
   }
 }
