163cbeb2b20f31cee6da6b2ce1f6cf0cc299a2ba
Gabriel Silvares <gabrielsrcode@gmail.com>
2021-05-29 16:51:50 -0300

    ✨ Adicionando a rota de Sessão

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { UserController } from './controllers/UserController';
+import { SessionController } from './controllers/SessionController';
 
 const routes = Router();
 
 const userController = new UserController();
+const sessionController = new SessionController();
 
+routes.post('/sessions', sessionController.create);
 routes.post('/users', userController.create);
 
 export default routes;
