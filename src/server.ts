8745ba251341e29d9e379e6a9e9e0f3cffb86703
Gabriel Silvares <gabrielsrcode@gmail.com>
2021-05-29 15:58:01 -0300

    ✨ Adicionando cors e errors padrão

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,8 @@
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-
-import routes from './routes';
+import express from 'express';
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
-app.use(routes);
-
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    return response.status(500).json({ message: error.message });
-  }
-);
 
 app.listen(3333, () => {
   console.log('Running in port 3333');
