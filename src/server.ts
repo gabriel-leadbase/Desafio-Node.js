6ec6467708108993691ba67f79a6297fe82f3f5a
Gabriel Silvares <gabrielsrcode@gmail.com>
2021-05-29 16:53:13 -0300

    ✨ Modificando o server para ter erro padrao e adicinando o cors e routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    return response.status(500).json({ message: error.message });
+  }
+);
 
 app.listen(3333, () => {
   console.log('Running in port 3333');
