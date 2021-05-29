e084dca31f258212ecb283d24ca085b5e88da9d0
Gabriel Silvares <gabrielsrcode@gmail.com>
2021-05-29 18:44:35 -0300

    ✨ Adicionando rota das permissões

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,17 @@ import { Router } from 'express';
 
 import { UserController } from './controllers/UserController';
 import { SessionController } from './controllers/SessionController';
+import { PermissionController } from './controllers/PermissionController';
 
 const routes = Router();
 
 const userController = new UserController();
 const sessionController = new SessionController();
+const permissionController = new PermissionController();
 
 routes.post('/sessions', sessionController.create);
 routes.post('/users', userController.create);
+
+routes.post('/permissions', permissionController.create);
 
 export default routes;
