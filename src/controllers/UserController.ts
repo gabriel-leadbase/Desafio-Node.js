a1fc7792b826d309adf3379b41b47ace1b626684
Gabriel Silvares <gabrielsrcode@gmail.com>
2021-05-29 16:01:26 -0300

    ✨ Adicionando o controller de criação do usuário

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { createUser } from '../useCases/createUser';
+
+export class UserController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { cpf, password } = request.body;
+
+    try {
+      const tokenAndUserData = await createUser({
+        cpfNumber: cpf,
+        password,
+      });
+
+      return response.status(201).json(tokenAndUserData);
+    } catch (error) {
+      return response.status(500).send();
+    }
+  }
+}
