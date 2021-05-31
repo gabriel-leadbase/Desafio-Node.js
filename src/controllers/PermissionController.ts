18cf11ed2a28778635c6f423605a0dfdc6ee9ac7
Gabriel Silvares <gabrielsrcode@gmail.com>
2021-05-31 03:31:36 -0300

    ✨ Feature de delete

--- src/controllers/PermissionController.ts
+++ src/controllers/PermissionController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { createPermission } from '../useCases/createPermission';
+import { deletePermission } from '../useCases/deletePermission';
 
 export class PermissionController {
   async create(request: Request, response: Response) {
@@ -9,6 +10,21 @@ export class PermissionController {
       const permissionData = await createPermission({ name, description });
 
       return response.json(permissionData);
+    } catch (error) {
+      console.log(`Error >> ${error}`);
+      return response.status(400).json({
+        message: 'Unexpected error',
+      });
+    }
+  }
+
+  async delete(request: Request, response: Response) {
+    const { name } = request.body;
+
+    try {
+      await deletePermission({ name });
+
+      return response.status(200).json();
     } catch (error) {
       console.log(`Error >> ${error}`);
       return response.status(400).json({
