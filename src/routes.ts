705fed65cb83935d827c5710a08a72d3f4293828
Gabriel Silvares <gabrielsrcode@gmail.com>
2021-05-31 00:59:49 -0300

    ✨ Adicionando rotas de usuario, login, permissão e cargo

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,21 @@
 import { Router } from "express";
 
-const router = Router();
+import { UserController } from './controllers/UserController';
+import { SessionController } from "./controllers/SessionController";
+import { PermissionController } from "./controllers/PermissionController";
+import { RoleController } from "./controllers/RoleContrloler";
 
-export { router };
+const routes = Router();
+
+const userController = new UserController();
+const sessionController = new SessionController();
+const permissionController = new PermissionController();
+const roleController = new RoleController();
+
+routes.post('/users', userController.create);
+routes.post("/sessions", sessionController.create);
+
+routes.post("/permissions", permissionController.create);
+routes.post("/roles", roleController.create);
+
+export { routes };
