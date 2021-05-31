bb7cb557edb36f87c5518f5816088b5bb270d0bc
Gabriel Silvares <gabrielsrcode@gmail.com>
2021-05-31 01:03:54 -0300

    ✨ Feature de permission

---
+++ src/useCases/createPermission.ts
@@ -0,0 +1,27 @@
+import { getCustomRepository } from "typeorm";
+import PermissionRepository from "../repositories/PermissionRepository";
+
+interface Request {
+  name: string;
+  description: string;
+}
+
+export async function createPermission({
+  name,
+  description,
+}: Request): Promise<void> {
+  const permissionRepository = getCustomRepository(PermissionRepository);
+
+  const existsPermission = await permissionRepository.findOne({ name });
+
+  if (existsPermission) {
+    throw new Error('Permission already exists.');
+  }
+
+  const permission = permissionRepository.create({
+    name,
+    description,
+  });
+
+  await permissionRepository.save(permission);
+}
