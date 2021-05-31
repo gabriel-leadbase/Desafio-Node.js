57bbdf2cfe18ddd7dbc43f7b786be19a67adc153
Gabriel Silvares <gabrielsrcode@gmail.com>
2021-05-31 03:30:35 -0300

    ✨ Adicionando middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 import { UserController } from './controllers/UserController';
-import { SessionController } from "./controllers/SessionController";
-import { PermissionController } from "./controllers/PermissionController";
-import { RoleController } from "./controllers/RoleContrloler";
+import { SessionController } from './controllers/SessionController';
+import { PermissionController } from './controllers/PermissionController';
+import { RoleController } from './controllers/RoleController';
+
+import { is } from './middlewares/permission';
 
 const routes = Router();
 
@@ -13,9 +15,11 @@ const permissionController = new PermissionController();
 const roleController = new RoleController();
 
 routes.post('/users', userController.create);
-routes.post("/sessions", sessionController.create);
+routes.post('/sessions', sessionController.create);
 
-routes.post("/permissions", permissionController.create);
-routes.post("/roles", roleController.create);
+routes.post('/roles', roleController.create);
+
+routes.post('/permissions', is(['ROLE_ADMIN']), permissionController.create);
+routes.delete('/permissions', is(['ROLE_ADMIN']), permissionController.delete);
 
 export { routes };
