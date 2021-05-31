50f42e0d12862dc06b6d6fcb7f8292a050326ed0
Gabriel Silvares <gabrielsrcode@gmail.com>
2021-05-31 01:01:01 -0300

    ✨ Criando o servidor

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import { router } from "./routes";
-import "reflect-metadata";
+import express from 'express';
+import { routes } from './routes';
+import 'reflect-metadata';
 
-import "./database";
+import './database';
 
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log("Running on port 3333");
+  console.log('Running on port 3333');
 });
